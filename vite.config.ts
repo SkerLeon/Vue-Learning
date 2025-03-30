6587fc099b19efa4a25f35bf9a483e001ef976d2
Leon <sker870812@gmail.com>
2025-03-30 17:51:14 +0800

    chore:20250330 安裝自動引入套件

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,10 +12,23 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    AutoImport({
+      imports: [
+        'vue', // 自動引入 vue hook
+        'vue-router', // 自動引入 router 方法
+        'pinia', // 如果需要，也可以引入 pinia
+      ],
+      dts: 'src/auto-imports.d.ts', // 生成自動引入的類型聲明
+      eslintrc: {
+        enabled: true, // 生成 eslint 設定
+        filepath: './.eslintrc-auto-import.json',
+        globalsPropValue: true,
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
